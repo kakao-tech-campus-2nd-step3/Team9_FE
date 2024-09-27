a6b4b6d8b3ac95c5b055fa08790be37690ea785e
seung365 <bdh3659@naver.com>
2024-09-27 22:34:50 +0900

    chore: CategoryTabElement 색상, 변수로 치환

--- src/components/CategoryTabElement.tsx
+++ src/components/CategoryTabElement.tsx
@@ -13,13 +13,14 @@ export default CategoryTabElement;
 
 const Wrapper = styled.div<{ color: 'white' | 'black' }>`
   background-color: var(--color-white);
-  color: black;
+  color: var(--color-black);
   width: 15.8rem;
   height: 4.4rem;
   font-size: var(--font-size-sm);
   border-bottom: ${({ color }) =>
-    color === 'white' ? '0.1rem solid var(--color-gray-02)' : '0.1rem solid black'};
+    color === 'white' ? '0.1rem solid var(--color-gray-02)' : '0.1rem solid var(--color-black)'};
   display: flex;
   justify-content: center;
   align-items: center;
+  border: 1px solid black;
 `;

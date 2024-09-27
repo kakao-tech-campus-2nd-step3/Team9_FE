e60f69fa301d05d3d722b40ee5aec480b756275f
joojjang <itchu999@naver.com>
2024-09-27 23:24:37 +0900

    chore: Chakra 프로바이더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ChakraProvider, ChakraProviderProps } from '@chakra-ui/react';
+
 import Routes from '@/routes';
 
-function App() {
+const App = () => {
+  const chakraProps: ChakraProviderProps = {};
+
   return (
-    <>
+    <ChakraProvider {...chakraProps}>
       <Routes />
-    </>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;

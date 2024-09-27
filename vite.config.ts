b3f52a2fd3acb95327abe4aa554fa23892c5ca22
joojjang <itchu999@naver.com>
2024-09-27 20:34:34 +0900

    chore(absolute paths): 디렉토리 추가 및 절대 경로 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,14 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@/apis', replacement: path.resolve(__dirname, 'src/apis') },
+      { find: '@/assets', replacement: path.resolve(__dirname, 'src/assets') },
       { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@/hooks', replacement: path.resolve(__dirname, 'src/hooks') },
       { find: '@/pages', replacement: path.resolve(__dirname, 'src/pages') },
       { find: '@/routes', replacement: path.resolve(__dirname, 'src/routes') },
       { find: '@/styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@/types', replacement: path.resolve(__dirname, 'src/types') },
       { find: '@/utils', replacement: path.resolve(__dirname, 'src/utils') },
     ],
   },

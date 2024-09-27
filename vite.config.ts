187685cc82e8f7987426c4fb6805644503190e97
joojjang <itchu999@naver.com>
2024-09-27 20:24:17 +0900

    chore: 디렉토리별로 절대 경로 설정
    - path 사용하기 위해 @types/node 설치
    - tsconfig.app.json과 vite.config.ts 파일에 경로 명시

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@/pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@/routes', replacement: path.resolve(__dirname, 'src/routes') },
+      { find: '@/styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@/utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
+  },
 });

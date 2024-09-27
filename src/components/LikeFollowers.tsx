d56fceb609893089e8a9fc08ff1e7d01244533b4
beom <74394824+seung365@users.noreply.github.com>
2024-09-27 22:51:23 +0900

    Feat/#9 컴포넌트 구현 4/9 완료 (#14)
    * chore: Prettier, ESLint 세팅
    
    * chore: CSS 초기화, App.tsx 초기화
    
    * feat: CategoryTabElement 구현
    
    * feat: CategoryTabs 구현
    
    * feat: FollowButton 구현
    
    * chore: index.html 언어 수정
    
    * chore: CategoryTabElement 색상, 변수로 치환
    
    * feat: ArtistItem 구현
    
    * design: CategoryTabs 폰트 색상 변경
    
    * feat:Thumbnail 구현
    
    * feat:LikeFollowers 구현
    
    * design: FollowButton border 색상 변경
    
    ---------
    
    Co-authored-by: joojjang <itchu999@naver.com>

---
+++ src/components/LikeFollowers.tsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'; // 속이 빈 하트 아이콘 임포트
+
+const LikeFollowers = () => {
+  return (
+    <Wrapper>
+      <LeftWrapper>
+        <FavoriteBorderIcon style={{ color: 'var(--color-gray-03)' }} />
+        <div>1K</div>
+      </LeftWrapper>
+      <Divider />
+      <RightWrapper>
+        <p style={{ color: 'var(--color-gray-03)' }}>팔로워</p>
+        <p>1.2K</p>
+      </RightWrapper>
+    </Wrapper>
+  );
+};
+
+export default LikeFollowers;
+
+const Wrapper = styled.div`
+  font-size: 1rem;
+  display: flex;
+  flex-direction: row;
+  width: 5.6rem;
+  height: 2.6rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LeftWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 1.2rem;
+  height: 2.6rem;
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 2.8rem;
+  height: 2.6rem;
+`;
+
+const Divider = styled.div`
+  width: 0.1rem;
+  height: 1.6rem;
+  background-color: var(--color-gray-03);
+  margin: 0 0.8rem;
+`;

7d0adfa0f91f342091a7771800954a8bdbcefd7b
seung365 <bdh3659@naver.com>
2024-10-01 15:03:06 +0900

    chore: MyGallery 폴더명 변경

--- src/components/ArtistItem/index.tsx
+++ src/components/ArtistItem/index.tsx
@@ -1,8 +1,7 @@
 import styled from '@emotion/styled';
 import { ThumbnailSquare } from './Thumbnail';
 import LikeFollowers from './LikeFollowers';
-import FollowButton from './FollowButton';
-
+import FollowButton from '../FollowButton';
 interface ArtistItemProps {
   themeNum: 'one' | 'two';
 }

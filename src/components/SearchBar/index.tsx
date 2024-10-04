73d0ebc5357595191a4540257eab1dd18264c5b4
joojjang <itchu999@naver.com>
2024-10-05 02:39:14 +0900

    feat(SearchBar): 아이콘 svg를 컴포넌트로 import 및 삽입 / favorite 유무 상태, cancel 버튼 유무 상태 구분
    - favorite 유무에 따라 인풋 요소 너비가 달라져서 InputBox 만들고 그 기준으로 아이콘 absolute로 위치시킴
    - 새 svg 파일 추가
    - includeFavorite와 searchWord 상태 구분하여 동적으로 요소 렌더링

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,24 +1,37 @@
 import styled from '@emotion/styled';
 import { HEADER_HEIGHT } from '../Header';
 import { useState } from 'react';
+import CancelIcon from '@/assets/icons/cancel-filled-gray.svg?react';
+import FavoriteIcon from '@/assets/icons/favorite-default.svg?react';
+import ArrowBackIcon from '@/assets/icons/arrow-back.svg?react';
+import SearchIcon from '@/assets/icons/search.svg?react';
 
 const SEARCH_PLACEHOLDER = '작품/작가 외 검색은 #을 붙여주세요';
 
-const SearchBar = () => {
+interface SearchBarProps {
+  includeFavorite?: boolean;
+}
+
+const SearchBar = ({ includeFavorite = false }: SearchBarProps) => {
   const [searchWord, setSearchWord] = useState<string>('');
 
   return (
     <SearchBarWrapper>
-      <span>arrow-back</span>
-      <Input
-        type="text"
-        placeholder={SEARCH_PLACEHOLDER}
-        value={searchWord}
-        onChange={(e) => {
-          setSearchWord(e.target.value);
-          //   console.log(e.target.value); // test
-        }}
-      />
+      <ArrowBackIcon />
+      <InputBox>
+        <StyledSearchIcon />
+        <Input
+          type="text"
+          placeholder={SEARCH_PLACEHOLDER}
+          value={searchWord}
+          onChange={(e) => {
+            setSearchWord(e.target.value);
+            //   console.log(e.target.value); // test
+          }}
+        />
+        {searchWord.trim().length > 0 && <StyledCancelIcon />}
+      </InputBox>
+      {includeFavorite && <FavoriteIcon />}
     </SearchBarWrapper>
   );
 };
@@ -40,19 +53,38 @@ const SearchBarWrapper = styled.div`
   gap: 10px;
 `;
 
-const Input = styled.input`
+const InputBox = styled.div`
+  position: relative;
   align-self: stretch;
   display: flex;
   align-items: center;
-  padding: 0px 8px;
-  gap: 8px;
   flex: 1 0 0;
   border-radius: 2px;
   border: 1px solid var(--color-gray-02);
   font-size: var(--font-size-xs);
+`;
+
+const Input = styled.input`
+  width: 100%;
+  align-self: stretch;
+  margin: 0 30px;
   outline: none;
 
   &::placeholder {
     color: var(--color-gray-03);
   }
+`;
+
+const StyledSearchIcon = styled(SearchIcon)`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  left: 8px;
+`;
+
+const StyledCancelIcon = styled(CancelIcon)`
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  right: 8px;
 `;

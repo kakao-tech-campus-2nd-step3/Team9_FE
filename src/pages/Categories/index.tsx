3fbb0f3206a242765c922ea45f139bf427e18b98
seung365 <bdh3659@naver.com>
2024-10-11 01:26:38 +0900

    refactor(CategoryData): CategoryData 모듈화

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -4,61 +4,14 @@ import styled from '@emotion/styled';
 import SearchBar from '@/components/SearchBar';
 import Gap from '@/components/layout/Gap';
 import { Text } from '@chakra-ui/react';
-
-const categories = [
-  {
-    id: 1,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '동양화/서양화',
-  },
-  {
-    id: 2,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '서양화/현대미술',
-  },
-  {
-    id: 3,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '민화/추상화',
-  },
-  {
-    id: 4,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '동양화/모던아트',
-  },
-  {
-    id: 5,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '서양화/클래식',
-  },
-  {
-    id: 6,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '수묵화/풍경화',
-  },
-  {
-    id: 7,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '동양화/인물화',
-  },
-  {
-    id: 8,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '서양화/정물화',
-  },
-  {
-    id: 9,
-    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
-    des: '동양화/서양화',
-  },
-];
+import categorydata from '@/apis/CategoryData';
 
 const Categories = () => {
   return (
     <Wrapper>
       <SearchBar includeFavorite={true} />
       <Grid col={4}>
-        {categories.map((category) => (
+        {categorydata.map((category) => (
           <Category key={category.id} src={category.src} des={category.des} />
         ))}
       </Grid>

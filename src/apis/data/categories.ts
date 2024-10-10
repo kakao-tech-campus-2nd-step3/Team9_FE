3820a4c9900c87b6e652c24f5e9e5e4826101f4c
Minjoo Kim <itchu999@naver.com>
2024-10-11 01:33:06 +0900

    rename(categories): apis/data/ 폴더 생성

--- src/apis/data/categories.ts
+++ src/apis/data/categories.ts
@@ -1,4 +1,4 @@
-const categorydata = [
+const categories = [
   {
     id: 1,
     src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
@@ -46,4 +46,4 @@ const categorydata = [
   },
 ];
 
-export default categorydata;
+export default categories;

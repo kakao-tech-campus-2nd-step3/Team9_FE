82e026df3aa14889dd7b63a39920bed3e26fe5d8
seung365 <bdh3659@naver.com>
2024-10-12 16:26:49 +0900

    !HOTFIX(ProtectedRoute): 리다이렉션 시 alert가 한번 더 뜨는 문제 해결

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate, Outlet } from 'react-router-dom';
 
 import { getDynamicPath } from './path';
+import { RouterPath } from './path';
 
 export const ProtectedRoute = () => {
   const accessToken = localStorage.getItem('accessToken');
@@ -9,8 +10,11 @@ export const ProtectedRoute = () => {
 
   // accessToken이 없으면 로그인 페이지로 리다이렉트, 있으면 자식 요소(페이지) 렌더링
   // 로그인 구현에 따라 추후 변동 가능성 O
+
   useEffect(() => {
-    if (!accessToken) {
+    const currentPath = window.location.pathname;
+    // 현재 경로가 로그인 페이지가 아닐 때만 경고 표시
+    if (!accessToken && currentPath !== `/${RouterPath.login}`) {
       alert('로그인 후 이용해주세요.');
       navigate(getDynamicPath.login());
     }

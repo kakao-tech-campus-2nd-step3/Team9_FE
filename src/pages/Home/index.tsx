a7a4e4c9057c6ead31bd8b8e7fe8c90a7af5429d
joojjang <itchu999@naver.com>
2024-10-12 17:20:15 +0900

    feat(Home): FABContainer 추가, 레이아웃 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,24 @@
+import styled from '@emotion/styled';
+
+import FABContainer from '@/components/FAB';
 import Footer from '@/components/Footer';
+import { HEADER_HEIGHT } from '@/components/Header';
+import { TABBAR_HEIGHT } from '@/components/TabBar';
 
 const Home = () => {
   return (
-    <>
+    <Wrapper className="home-contents">
       Home
       <Footer />
-    </>
+      <FABContainer mode="seller" />
+    </Wrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - ${HEADER_HEIGHT} - ${TABBAR_HEIGHT});
+  display: flex;
+  flex-direction: column;
+`;

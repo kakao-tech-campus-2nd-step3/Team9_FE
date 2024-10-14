09f6f312db10a8d454e7b9c73e14f18c765b25bf
seung365 <bdh3659@naver.com>
2024-10-14 16:26:32 +0900

    feat(RecentSearch): 모두 삭제하기 버튼 추가

--- src/components/common/SearchModal/RecentSearch.tsx
+++ src/components/common/SearchModal/RecentSearch.tsx
@@ -1,9 +1,15 @@
 import styled from '@emotion/styled';
 import Chip from '../Chip';
 import { useState, useEffect } from 'react';
+import { Text } from '@chakra-ui/react';
 
 const RecentSearch = () => {
   const [searchArray, setSearchArray] = useState([]);
+
+  const allDelete = () => {
+    setSearchArray([]);
+    localStorage.removeItem('searchArray');
+  };
 
   const handleStoredData = (tag: string) => {
     const updatedArray = searchArray.filter((item) => item !== tag);
@@ -20,10 +26,15 @@ const RecentSearch = () => {
 
   return (
     <Wrapper>
-      <h2>최근 검색어</h2>
-      {searchArray.map((item, index) => (
-        <Chip key={index} tag={item} onClick={() => handleStoredData(item)} />
-      ))}
+      <TitleWrapper>
+        <TitleText>최근 검색어</TitleText>
+        {searchArray.length > 0 && <DelText onClick={allDelete}>모두 삭제하기</DelText>}
+      </TitleWrapper>
+      <ChipWrapper>
+        {searchArray.map((item, index) => (
+          <Chip key={index} tag={item} onClick={() => handleStoredData(item)} />
+        ))}
+      </ChipWrapper>
     </Wrapper>
   );
 };
@@ -32,5 +43,35 @@ export default RecentSearch;
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  padding: 16px;
+`;
+
+const ChipWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`;
+
+const DelText = styled(Text)`
+  color: var(--color-gray-deep, #909090);
+  text-align: right;
+  font-size: var(--font-size-sm);
+  font-weight: 400;
+  line-height: normal;
+  cursor: pointer;
+`;
+
+const TitleText = styled(Text)`
+  color: var(--color-black, #020715);
+  font-size: var(--font-size-md);
+  font-weight: 700;
+  line-height: normal;
 `;

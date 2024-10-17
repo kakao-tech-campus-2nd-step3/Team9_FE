2f39c35a067eeb4e1cc4e40605fa72d1ee00c9e5
seung365 <bdh3659@naver.com>
2024-10-17 09:47:56 +0900

    chore(data): ad 목데이터 추가

---
+++ src/apis/data/ad.ts
@@ -0,0 +1,12 @@
+const ad = [
+  {
+    id: 1,
+    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
+  },
+  {
+    id: 2,
+    src: 'https://i.pinimg.com/474x/45/37/93/4537932e76ebcc6186f86b75d9eeac87.jpg',
+  },
+];
+
+export default ad;

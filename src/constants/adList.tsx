08dc51edb65d9768eeb66524b341b9c987c0dfac
joojjang <itchu999@naver.com>
2024-10-18 04:22:15 +0900

    chore(adList): 문구 변경

--- src/constants/adList.tsx
+++ src/constants/adList.tsx
@@ -7,7 +7,7 @@ export type Ad = {
 export const adList: Ad[] = [
   {
     image: 'https://ilmin.org/wp-content/uploads/2023/04/20230411_010038-scaled.jpg',
-    title: '리얼리즘으로 회화의 가능성을 모색하다',
+    title: '《히스테리아: 동시대 리얼리즘 회화》',
     description: '동시대 작가 13인의 작업을 통해 본 회화의 ‘리얼’한 경향',
   },
   {
@@ -19,6 +19,6 @@ export const adList: Ad[] = [
   {
     image: 'https://diaf.or.kr/img/diaf/msec02.jpg',
     title: '제 17회 대구국제아트페어',
-    description: '티켓 사전 구매 시 경품 증정',
+    description: '티켓 사전 구매 시 추첨을 통해 경품 증정',
   },
 ];

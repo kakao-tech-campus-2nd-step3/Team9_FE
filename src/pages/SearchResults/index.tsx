8f5b9f5c3354183bbd7439bb3572689e4e7726a7
seung365 <bdh3659@naver.com>
2024-10-17 12:36:45 +0900

    feat(SearchResults): swiper UI 추가

--- src/pages/SearchResults/index.tsx
+++ src/pages/SearchResults/index.tsx
@@ -1,13 +1,25 @@
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import SearchBar from '@/components/layouts/SearchBar';
+import CategoryTabBar from './components/CategoryTabBar';
+import SwiperFrame from './components/SwiperFrame';
+import searchWork from '@/apis/data/searchWork';
+import searchArtist from '@/apis/data/searchArtist';
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
 
   return (
     <>
-      SearchResults
-      <h1>{query}</h1>
+      <SearchBar goBack={goBack} />
+      <CategoryTabBar /> <h1>query : {query}</h1>
+      <SwiperFrame children={searchWork} />
+      <SwiperFrame children={searchArtist} />
     </>
   );
 };

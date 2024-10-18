f9003f2754e80d7e029bc17116ef0e80e3220e9d
seung365 <bdh3659@naver.com>
2024-10-18 20:48:37 +0900

    feat(NoHeaderLayout): HeaderLayout 수정에 맞춰 수정

--- src/components/layouts/NoHeaderLayout/index.tsx
+++ src/components/layouts/NoHeaderLayout/index.tsx
@@ -1,18 +1,35 @@
+import styled from '@emotion/styled';
+import { useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import FABContainer from '@/components/layouts/FAB';
 import TabBar from '@/components/layouts/TabBar';
 
-const NoHeaderLayout = () => {
+const BasicLayout = () => {
   const mode = 'seller';
+  const contentWrapperRef = useRef<HTMLDivElement>(null);
 
   return (
-    <>
-      <Outlet />
-      <FABContainer mode={mode} />
+    <PageLayout>
+      <ContentWrapper ref={contentWrapperRef}>
+        <Outlet />
+      </ContentWrapper>
+      <FABContainer mode={mode} scrollContainerRef={contentWrapperRef} />
       <TabBar />
-    </>
+    </PageLayout>
   );
 };
 
-export default NoHeaderLayout;
+export default BasicLayout;
+
+const PageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const ContentWrapper = styled.main`
+  flex: 1;
+  overflow-y: auto;
+  display: flex;
+`;

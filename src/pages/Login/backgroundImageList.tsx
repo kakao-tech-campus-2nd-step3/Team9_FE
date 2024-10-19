17e65e18fa8da3ca0505fb360142d1ca6dc54d22
joojjang <itchu999@naver.com>
2024-10-19 13:46:24 +0900

    refactor(Login): 배경이미지 파일 빼고 랜덤으로 렌더링

---
+++ src/pages/Login/backgroundImageList.tsx
@@ -0,0 +1,19 @@
+export const backgroundImageList: { src: string; creator: string }[] = [
+  {
+    src: 'https://images.unsplash.com/photo-1653315215217-ac5b8dde9cd3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    creator: '',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1649115727835-0d8eee476bdc?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    creator: '',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1715267264107-4d6a0a80852f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    creator: '',
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1645460223610-39a23a7022a4?q=80&w=2950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    creator:
+      '사진: <a href="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EB%81%9D%EC%97%90-%EB%B9%9B%EC%9D%B4-%EC%9E%88%EB%8A%94-%EB%82%98%EC%84%A0%ED%98%95-%EA%B3%84%EB%8B%A8-iV-Fyo7_Neg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>의<a href="https://unsplash.com/ko/@vinsen_cekrani?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vinsen Cekrani</a>',
+  },
+];
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,36 +1,21 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import { Route, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import KakaoSymbol from '@/assets/kakao-symbol.svg?react';
 import Logo from '@/assets/logo.svg?react';
+import IconButton from '@/components/common/IconButton';
 import Header, { HEADER_HEIGHT } from '@/components/layouts/Header';
 import { RouterPath } from '@/routes/path';
-import IconButton from '@/components/common/IconButton';
-
-const backgroundImageList: { src: string; creator: string }[] = [
-  {
-    src: 'https://images.unsplash.com/photo-1653315215217-ac5b8dde9cd3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    creator: '',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1649115727835-0d8eee476bdc?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    creator: '',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1715267264107-4d6a0a80852f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    creator: '',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1645460223610-39a23a7022a4?q=80&w=2950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    creator:
-      '사진: <a href="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EB%81%9D%EC%97%90-%EB%B9%9B%EC%9D%B4-%EC%9E%88%EB%8A%94-%EB%82%98%EC%84%A0%ED%98%95-%EA%B3%84%EB%8B%A8-iV-Fyo7_Neg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>의<a href="https://unsplash.com/ko/@vinsen_cekrani?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vinsen Cekrani</a>',
-  },
-];
+import { backgroundImageList } from './backgroundImageList';
 
 const Login = () => {
   const isMember = false; // 추후 API 연동
   const navigate = useNavigate();
+
+  // 랜덤 배경이미지
+  const randomIndex = Math.floor(Math.random() * backgroundImageList.length);
+  const backgroundImage = backgroundImageList[randomIndex].src;
 
   const handleLogin = () => {
     if (!isMember) {
@@ -39,7 +24,7 @@ const Login = () => {
   };
 
   return (
-    <Wrapper backgroundImage={backgroundImageList[0].src}>
+    <Wrapper backgroundImage={backgroundImage}>
       <Header
         mode="seller"
         leftSideChildren={

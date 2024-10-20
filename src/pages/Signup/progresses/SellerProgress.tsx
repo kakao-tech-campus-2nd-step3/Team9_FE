bb4ab6711cffda9e3aa72c4e7accbfff033ee044
joojjang <itchu999@naver.com>
2024-10-21 03:05:15 +0900

    feat(SellerProgress): 이름 변수 추가

--- src/pages/Signup/progresses/SellerProgress.tsx
+++ src/pages/Signup/progresses/SellerProgress.tsx
@@ -1,23 +1,27 @@
 import ProgressBar from '../ProgressBar';
 import { ProgressBox } from './styles';
 
-const SellerProgress = () => (
-  <>
-    <ProgressBox>
-      <ProgressBar percentage={75} />
-      <p className="progress-guidance">
-        000 님, 반가워요.
-        <br />
-        작가 유형을 선택해주세요.
-      </p>
-      <form></form>
-    </ProgressBox>
-    <ProgressBox>
-      <ProgressBar percentage={100} />
-      <p className="progress-guidance">작가 정보를 입력해주세요.</p>
-      <form></form>
-    </ProgressBox>
-  </>
-);
+const SellerProgress = () => {
+  const name = '000';
+
+  return (
+    <>
+      <ProgressBox>
+        <ProgressBar percentage={75} />
+        <p className="progress-guidance">
+          {name} 님, 반가워요.
+          <br />
+          작가 유형을 선택해주세요.
+        </p>
+        <form className="progress-form"></form>
+      </ProgressBox>
+      <ProgressBox>
+        <ProgressBar percentage={100} />
+        <p className="progress-guidance">작가 정보를 입력해주세요.</p>
+        <form className="progress-form"></form>
+      </ProgressBox>
+    </>
+  );
+};
 
 export default SellerProgress;

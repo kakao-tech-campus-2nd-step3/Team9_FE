d7ff38f90fdbabd79ba241758920d096e9af4aa1
joojjang <itchu999@naver.com>
2024-10-26 17:21:50 +0900

    design(BusinessProgress): 글씨 크기, 위치 수정

--- src/pages/Signup/progresses/SellerProgress/BusinessProgress.tsx
+++ src/pages/Signup/progresses/SellerProgress/BusinessProgress.tsx
@@ -1,5 +1,9 @@
-import { Box } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 
-const BusinessProgress = () => <Box>추후 서비스 제공 예정입니다.</Box>;
+const BusinessProgress = () => (
+  <Text fontSize="var(--font-size-sm)" alignSelf="center">
+    추후 서비스 제공 예정입니다.
+  </Text>
+);
 
 export default BusinessProgress;

e9888fac7f8917a662e8a82d089fafea0424cef3
joojjang <itchu999@naver.com>
2024-10-28 04:00:14 +0900

    style(SellerProgress): 공백 라인 추가

--- src/pages/Signup/progresses/SellerProgress/index.tsx
+++ src/pages/Signup/progresses/SellerProgress/index.tsx
@@ -13,6 +13,7 @@ const SellerProgress = () => {
   const [currentStep, setCurrentStep] = useState<1 | 2>(1);
 
   const goToStep2 = () => setCurrentStep(2);
+
   return (
     <>
       <ProgressBox>

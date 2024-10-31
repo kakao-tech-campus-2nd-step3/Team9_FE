5d336bd7996ba03c76189b67ee41044e641d2d45
seung365 <bdh3659@naver.com>
2024-10-31 14:11:23 +0900

    feat(MoreButton): MoreButton 컴포넌트 생성

---
+++ src/pages/SearchResults/components/MoreButton.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+const MoreButton = styled.button`
+  display: flex;
+  width: 328px;
+  height: 42px;
+  min-height: 42px;
+  padding: 4px 8px;
+  margin: 16px 0px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 2px;
+  border: 1px solid var(--color-gray-md);
+  background: var(--color-white);
+`;
+
+export default MoreButton;

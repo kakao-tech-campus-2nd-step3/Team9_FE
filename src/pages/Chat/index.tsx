9d261e1ca8f86db974e47a485c386ca4584e8ec4
joojjang <itchu999@naver.com>
2024-11-02 00:15:17 +0900

    feat(Chat): 채팅 페이지 레이아웃 대충

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,5 +1,26 @@
+import styled from '@emotion/styled';
+
+import Header, { HEADER_HEIGHT } from '@/components/layouts/Header';
+import { TABBAR_HEIGHT } from '@/components/layouts/TabBar';
+import IconButton from '@/components/common/IconButton';
+import { useNavigate } from 'react-router-dom';
+
 const Chat = () => {
-  return <>Chat</>;
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <Header leftSideChildren={<IconButton icon="arrow-back" onClick={() => navigate(-1)} />} />
+      {/* 채팅 목록 */}
+    </Wrapper>
+  );
 };
 
 export default Chat;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin: ${HEADER_HEIGHT} 0 ${TABBAR_HEIGHT} 0;
+`;

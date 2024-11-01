18263fb559c4cf711707b0b594979edd6dcd0738
seung365 <bdh3659@naver.com>
2024-11-02 03:30:05 +0900

    feat(Swiper): 검색 페이지 Swiper navigation 기능 추가 및 UI 수정

--- src/pages/SearchResults/components/SwiperFrame.tsx
+++ src/pages/SearchResults/components/SwiperFrame.tsx
@@ -1,41 +1,48 @@
 import { SearchArtist, SearchWork } from '@/types/index';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Scrollbar } from 'swiper/modules';
+import { Scrollbar, Navigation } from 'swiper/modules';
+import ProductItem from '@/components/common/ProductItem';
+import ArtistItem from '@/components/common/ArtistItem';
 
 import styled from '@emotion/styled';
 
 import 'swiper/css';
-import 'swiper/css/scrollbar'; // Add this line to import scrollbar styles
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
 
 interface SwiperFrame {
   children: SearchArtist[] | SearchWork[];
 }
 
+//     const ArtistItem: ({ author, like, follower, size, src, alt }: ArtistItemProps)
+
 const SwiperFrame = ({ children }: SwiperFrame) => {
   return (
     <SwiperWrapper
-      modules={[Scrollbar]}
-      scrollbar={{ draggable: true }}
-      slidesPerView={3}
-      spaceBetween={40}
+      modules={[Scrollbar, Navigation]} // Include Navigation here
+      slidesPerView="auto"
+      spaceBetween={3}
+      navigation={true}
     >
       {children.map((item) => (
         <StyledSwiperSlide key={item.id}>
-          <Image src={item.src} alt={'title' in item ? item.title : item.name} />
           {'title' in item && (
-            <>
-              <p>제목: {item.title}</p>
-              <p>작가: {item.artist}</p>
-              <p>가격: {item.price}원</p>
-            </>
+            <ProductItem
+              author={item.artist}
+              title={item.title}
+              src={item.src}
+              price={item.price}
+              key={item.id}
+            />
           )}
           {'name' in item && (
-            <>
-              <p>이름: {item.name}</p>
-              <p>팔로워 수: {item.totalFollowers}</p>
-              <p>좋아요 수: {item.totalLikes}</p>
-              <p>{item.followed ? '팔로우 중' : '팔로우 안 함'}</p>
-            </>
+            <ArtistItem
+              author={item.name}
+              src={item.src}
+              like={item.totalLikes}
+              follower={item.totalFollowers}
+              key={item.id}
+            />
           )}
         </StyledSwiperSlide>
       ))}
@@ -48,16 +55,30 @@ export default SwiperFrame;
 const SwiperWrapper = styled(Swiper)`
   width: 100%;
   height: 223px;
-  padding: 10px 0;
+  padding: 0 16px;
+  .swiper-button-next,
+  .swiper-button-prev {
+    color: #333;
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    padding: 5px;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+
+  .swiper-button-next {
+    right: 10px;
+  }
+
+  .swiper-button-prev {
+    left: 10px;
+  }
 `;
 
 const StyledSwiperSlide = styled(SwiperSlide)`
   width: 180px;
   height: 144px;
-`;
-
-const Image = styled.img`
-  width: 140px;
-  height: 140px;
-  object-fit: cover;
 `;

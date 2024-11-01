2454f0d7c88b3b76e7493f2d99d3bcfa2f34cd50
seung365 <bdh3659@naver.com>
2024-11-02 03:35:54 +0900

    feat(SearchPage): 검색 페이지 구현

--- src/pages/SearchResults/index.tsx
+++ src/pages/SearchResults/index.tsx
@@ -1,28 +1,100 @@
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import SearchBar from '@/components/layouts/SearchBar';
 import CategoryTabBar from './components/CategoryTabBar';
 import SwiperFrame from './components/SwiperFrame';
 import searchWork from '@/apis/data/searchWork';
 import searchArtist from '@/apis/data/searchArtist';
 import { RouterPath } from '@/routes/path';
+import { useState } from 'react';
+import ArtWorkContents from './components/ArtWorkContents';
+import ArtistContents from './components/ArtistContents';
+import styled from '@emotion/styled';
+import Gap from '@/components/styles/Gap';
+import MoreButton from './components/MoreButton';
 
 const SearchResults = () => {
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get('query');
+  const [selectedTab, setSelectedTab] = useState('전체');
   const navigate = useNavigate();
+  const searchLen = searchWork.length + searchArtist.length;
+  const searchWorkLen = searchWork.length;
+  const searchArtistLen = searchArtist.length;
 
   const goBack = () => {
     navigate(RouterPath.categories);
   };
 
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <>
       <SearchBar goBack={goBack} />
-      <CategoryTabBar /> <h1>query : {query}</h1>
-      <SwiperFrame children={searchWork} />
-      <SwiperFrame children={searchArtist} />
+      <CategoryTabBar tabClick={handleTabClick} tabState={selectedTab} />
+      <div>
+        {selectedTab === '전체' && (
+          <div>
+            <ResultFont>{searchLen}건의 결과</ResultFont>
+            <SubTitleFont>
+              작품 <ResultLightFont>({searchWorkLen})</ResultLightFont>
+            </SubTitleFont>
+            <SwiperWrapper>
+              <SwiperFrame children={searchWork}></SwiperFrame>
+              <MoreButton onClick={() => handleTabClick('작품')}> 더보기 </MoreButton>
+            </SwiperWrapper>
+            <Gap height={12} />
+            <SubTitleFont>
+              작가 <ResultLightFont>({searchArtistLen})</ResultLightFont>
+            </SubTitleFont>
+            <SwiperWrapper>
+              <SwiperFrame children={searchArtist}></SwiperFrame>
+              <MoreButton onClick={() => handleTabClick('작가')}> 더보기 </MoreButton>
+            </SwiperWrapper>
+          </div>
+        )}
+        {selectedTab === '작품' && <ArtWorkContents />}
+        {selectedTab === '작가' && <ArtistContents />}
+      </div>
     </>
   );
 };
 
 export default SearchResults;
+
+const ResultFont = styled.div`
+  color: var(--color-black, #020715);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  display: flex;
+  flex-direction: row;
+  padding: 8px 16px;
+`;
+
+const SubTitleFont = styled.div`
+  color: var(--color-black, #020715);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  display: flex;
+  flex-direction: row;
+  padding: 16px 16px;
+`;
+
+const ResultLightFont = styled.div`
+  color: var(--color-black, #020715);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-left: 2px;
+`;
+
+const SwiperWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;

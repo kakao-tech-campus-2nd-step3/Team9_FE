eddfab0cf538bad55f087f1dc31de03f551027d2
joojjang <itchu999@naver.com>
2024-11-02 00:39:25 +0900

    feat(ChatItem): 채팅 목록의 아이템 UI 구현

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,15 @@
 import styled from '@emotion/styled';
 
+import IconButton from '@/components/common/IconButton';
 import Header, { HEADER_HEIGHT } from '@/components/layouts/Header';
 import { TABBAR_HEIGHT } from '@/components/layouts/TabBar';
-import IconButton from '@/components/common/IconButton';
 import { useNavigate } from 'react-router-dom';
+import ChatItem from './components/ChatItem';
+
+const chatList = [
+  { imageUrl: '', nickname: '미니멀앤', date: '2024-11-01', content: '네. 알겠습니다.' },
+  { imageUrl: '', nickname: '뾰롱', date: '2024-10-31', content: '네. 그렇게 해요.' },
+];
 
 const Chat = () => {
   const navigate = useNavigate();
@@ -11,7 +17,15 @@ const Chat = () => {
   return (
     <Wrapper>
       <Header leftSideChildren={<IconButton icon="arrow-back" onClick={() => navigate(-1)} />} />
-      {/* 채팅 목록 */}
+      {chatList.map((item, index) => (
+        <ChatItem
+          key={index}
+          imageUrl={item.imageUrl}
+          nickname={item.nickname}
+          date={item.date}
+          content={item.content}
+        />
+      ))}
     </Wrapper>
   );
 };

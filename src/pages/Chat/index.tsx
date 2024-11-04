56a3e6e8e0fa774e527ac497f1bf4d549e34cdb2
joojjang <itchu999@naver.com>
2024-11-04 19:37:48 +0900

    fix(Chat): 훅은 함수 컴포넌트 안에서 호출

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -6,12 +6,12 @@ import Header, { HEADER_HEIGHT } from '@/components/layouts/Header';
 import { TABBAR_HEIGHT } from '@/components/layouts/TabBar';
 import ChatItem from './components/ChatItem';
 
-const [chatList, setChatList] = useState([
-  { imageUrl: '', nickname: '미니멀앤', date: '2024-11-01', content: '네. 알겠습니다.' },
-  { imageUrl: '', nickname: '뾰롱', date: '2024-10-31', content: '네. 그렇게 해요.' },
-]);
-
 const Chat = () => {
+  const [chatList, setChatList] = useState([
+    { imageUrl: '', nickname: '미니멀앤', date: '2024-11-01', content: '네. 알겠습니다.' },
+    { imageUrl: '', nickname: '뾰롱', date: '2024-10-31', content: '네. 그렇게 해요.' },
+  ]);
+
   return (
     <Wrapper>
       <Header rightSideChildren={<IconButton icon="settings" />} />

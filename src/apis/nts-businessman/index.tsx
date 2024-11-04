0fe3ebe186e9e8316724d1bfddb79028507342bc
joojjang <itchu999@naver.com>
2024-11-04 12:26:01 +0900

    fix(nts-businessman): proxy 주소 변경

--- src/apis/nts-businessman/index.tsx
+++ src/apis/nts-businessman/index.tsx
@@ -1,4 +1,4 @@
 export const API_KEY = import.meta.env.VITE_APP_NTS_BUSINESSMAN_API_KEY;
 
-const CORS_ENYWHERE = `https://cors-anywhere.herokuapp.com/`;
-export const BASE_URL = `${CORS_ENYWHERE}https://api.odcloud.kr/api/nts-businessman/v1`;
+const CORS_PROXY = `https://cors.bridged.cc/`;
+export const BASE_URL = `${CORS_PROXY}https://api.odcloud.kr/api/nts-businessman/v1`;

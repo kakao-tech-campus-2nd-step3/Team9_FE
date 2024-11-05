574770927b8bda232e75f1e15e582580eda975a2
joojjang <itchu999@naver.com>
2024-11-05 16:45:57 +0900

    feat(SearchBar): 서치 바 포지션 fixed로 변경, z-index 설정

--- src/pages/Discover/index.tsx
+++ src/pages/Discover/index.tsx
@@ -1,5 +1,22 @@
+import styled from '@emotion/styled';
+
+import SearchBar from '@/components/layouts/SearchBar';
+import { HEADER_HEIGHT } from '@/components/layouts/Header';
+import { TABBAR_HEIGHT } from '@/components/layouts/TabBar';
+
 const Discover = () => {
-  return <>Discover</>;
+  return (
+    <Wrapper>
+      <SearchBar />
+    </Wrapper>
+  );
 };
 
 export default Discover;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin: ${HEADER_HEIGHT} 0 ${TABBAR_HEIGHT} 0;
+`;

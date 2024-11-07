35d208a4e97adc96ee5441d398523cdbfe243e56
seung365 <bdh3659@naver.com>
2024-11-08 01:25:30 +0900

    feat(page): My page 구현

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,12 +1,37 @@
+import { useGetUser } from '@/apis/users/useGetUser';
 import Footer from '@/components/layouts/Footer';
+import styled from '@emotion/styled';
+import MenuSection from './components/MenuSection';
+import ProfileBox from './components/ProfileBox';
 
 const My = () => {
+  const { data, isLoading, isError } = useGetUser();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (isError || !data) {
+    return <p>Error... console.log('Error:', error);</p>;
+  }
   return (
     <>
-      My
+      <ProfileSection>
+        <ProfileBox
+          userImageUrl={data?.data.userImageUrl}
+          hashTag={data?.data.hashTags}
+          userName={data?.data.username}
+        />
+      </ProfileSection>
+      <MenuSection />
       <Footer />
+      <div style={{ marginBottom: '53px' }} />
     </>
   );
 };
 
 export default My;
+
+const ProfileSection = styled.div`
+  background: var(--color-black);
+  padding: 16px;
+`;

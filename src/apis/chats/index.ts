6a24699f69b92888c56494e0731cc191a3153dbd
joojjang <itchu999@naver.com>
2024-11-07 13:10:49 +0900

    feat(chats API): index 파일 생성

---
+++ src/apis/chats/index.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL_CHAT;
+
+// todo: 엔드 포인트 연결
+// /sub/chat/rooms/{chatRoomId} //구독
+// /pub/chat/{chatRoomId} //메시지 전송
+// /pub/chat/{chatRoomId}/file //파일 전송

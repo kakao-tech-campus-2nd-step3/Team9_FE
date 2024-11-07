93391beb8076cec589fdef9325fa8d378990a506
joojjang <itchu999@naver.com>
2024-11-07 13:54:15 +0900

    style(utils): formatDate 함수 형태 수정 (화살표 안 쓰고 function 키워드 사용)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,10 @@
 // 날짜 포맷 함수
-export const formatDate = (dateStr: string): string => {
+export function formatDate(dateStr: string): string {
   const dateObj = new Date(dateStr);
 
   return `${dateObj.getFullYear()}.${String(dateObj.getMonth() + 1).padStart(2, '0')}.${String(dateObj.getDate()).padStart(2, '0')}`;
   // 2024.00.00 형식으로 반환
-};
+}
 
 // 공백 제거 함수
 export function eliminateSpaces(str: string): string {

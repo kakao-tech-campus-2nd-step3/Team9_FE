5f5bf32b01bcb8f2eec60079df949cbc80f18122
seung365 <bdh3659@naver.com>
2024-11-08 22:24:50 +0900

    feat(api): useGetUser user일때와 artist에 따라 api 호출 다르게

--- src/apis/users/useGetUser.ts
+++ src/apis/users/useGetUser.ts
@@ -1,11 +1,15 @@
-import { APIResponse, MyInfo } from '@/types';
+import { APIResponse, ArtistInfo, UserInfo } from '@/types';
 import { useQuery } from '@tanstack/react-query';
 import { fetchInstance } from '../instance';
 
 const token = localStorage.getItem('accessToken');
 
-export const getUser = async (): Promise<APIResponse<MyInfo>> => {
-  const response = await fetchInstance().get('/users', {
+type UserMode = 'user' | 'artist';
+type InfoType<T> = T extends 'user' ? UserInfo : ArtistInfo;
+
+export const getUser = async <T extends UserMode>(mode: T): Promise<APIResponse<InfoType<T>>> => {
+  const endpoint = mode === 'user' ? '/users' : '/artists';
+  const response = await fetchInstance().get(endpoint, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -13,10 +17,10 @@ export const getUser = async (): Promise<APIResponse<MyInfo>> => {
   return response.data;
 };
 
-export const useGetUser = () => {
-  const { data, isLoading, isError } = useQuery<APIResponse<MyInfo>, Error>({
-    queryKey: ['userInfo'],
-    queryFn: getUser,
+export const useGetUser = <T extends UserMode>(mode: T) => {
+  const { data, isLoading, isError } = useQuery<APIResponse<InfoType<T>>, Error>({
+    queryKey: ['userInfo', mode],
+    queryFn: () => getUser(mode),
   });
 
   return { data, isLoading, isError };

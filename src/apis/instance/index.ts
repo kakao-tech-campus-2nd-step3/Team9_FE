e659ab5688acd88a7ed1cb42ceac94b9386c4993
joojjang <itchu999@naver.com>
2024-11-10 13:40:26 +0900

    refactor(API instance): API 요청 시 최신 토큰 유지

--- src/apis/instance/index.ts
+++ src/apis/instance/index.ts
@@ -2,7 +2,6 @@ import { QueryClient } from '@tanstack/react-query';
 import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
-const token = localStorage.getItem('accessToken');
 
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
@@ -11,8 +10,18 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
     headers: {
       'Content-Type': 'application/json',
       'Cross-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${token}`,
     },
+  });
+
+  // API 요청 시마다 최신 토큰을 가져옴
+  instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
   });
 
   return instance;

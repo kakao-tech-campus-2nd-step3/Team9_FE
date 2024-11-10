5f51ef5b26cf1d20877963fe093c769bdb0da5d2
joojjang <itchu999@naver.com>
2024-11-10 20:58:00 +0900

    feat(ProductDetails): 페이지 간단하게 구현 (채팅하기 버튼만)

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,5 +1,39 @@
+import usePostChatRoom from '@/apis/chats/usePostChatRoom';
+import CTA, { CTAContainer } from '@/components/common/CTA';
+
+const USER_EMAIL_1 = 'ble6859@knu.ac.kr';
+const USER_EMAIL_2 = 'abc@knu.ac.kr';
+
 const ProductDetails = () => {
-  return <>ProductDetails</>;
+  const userEmail1 = USER_EMAIL_1; // 사용자 본인 이메일
+  const userEmail2 = USER_EMAIL_2; // 상대방 이메일
+
+  const { mutate: postChatRoom } = usePostChatRoom();
+
+  const handleClickChat = () => {
+    postChatRoom(
+      {
+        userEmail1,
+        userEmail2,
+      },
+      {
+        onSuccess: () => {
+          alert('채팅방 생성 완료!');
+        },
+        onError: (error) => {
+          alert(error);
+        },
+      },
+    );
+  };
+
+  return (
+    <>
+      <CTAContainer>
+        <CTA label="채팅하기" onClick={handleClickChat}></CTA>
+      </CTAContainer>
+    </>
+  );
 };
 
 export default ProductDetails;

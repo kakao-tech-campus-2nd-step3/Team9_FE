15872666eb39391d83f49825ae71ec2c2a38393f
beom <bdh3659@naver.com>
2024-11-10 21:08:04 +0900

    refactor(MenuSection): MenuItem 리스트 만들어 map으로 처리

--- src/pages/My/components/MenuSection/UserMenuSection.tsx
+++ src/pages/My/components/MenuSection/UserMenuSection.tsx
@@ -2,14 +2,23 @@ import { RouterPath } from '@/routes/path';
 import { useNavigate } from 'react-router-dom';
 import { MenuItem, UlWrapper, Wrapper } from './styles';
 
+const menuItems = [
+  { label: '구매 내역', path: RouterPath.orders },
+  { label: '찜 / 팔로우', path: RouterPath.favorites },
+  { label: '회원 정보 수정', path: '' },
+];
+
 const UserMenuSection = () => {
   const navigate = useNavigate();
+
   return (
     <Wrapper>
       <UlWrapper>
-        <MenuItem onClick={() => navigate(RouterPath.orders)}>구매 내역</MenuItem>
-        <MenuItem onClick={() => navigate(RouterPath.favorites)}>찜 / 팔로우</MenuItem>
-        <MenuItem>회원 정보 수정</MenuItem>
+        {menuItems.map((item, index) => (
+          <MenuItem key={index} onClick={() => item.path && navigate(item.path)}>
+            {item.label}
+          </MenuItem>
+        ))}
       </UlWrapper>
     </Wrapper>
   );

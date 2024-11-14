289e539b8842baeb2bec87f97bba79758305217d
beom <bdh3659@naver.com>
2024-11-15 01:00:00 +0900

    feat(queryKeys): queryKey 검색 리스트 추가

--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -2,6 +2,8 @@ const QUERY_KEYS = {
   FEED: 'feed',
   FOLLOW_LIST: 'followList',
   USER_INFO: 'userInfo',
+  ARTIST_LIST: 'artistList',
+  PRODUCT_LIST: 'productList',
 };
 
 export default QUERY_KEYS;

09c36f8f84d70ca960c41ef48d10ca76ab976b9d
joojjang <itchu999@naver.com>
2024-11-15 13:52:02 +0900

    chore(ProtectedRoute): todo 주석 작성

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -9,7 +9,7 @@ export const ProtectedRoute = () => {
   const navigate = useNavigate();
 
   // accessToken이 없으면 로그인 페이지로 리다이렉트, 있으면 자식 요소(페이지) 렌더링
-  // 로그인 구현에 따라 추후 변동 가능성 O
+  // todo: 로그인 구현에 따라 추후 변동 가능성 O
 
   useEffect(() => {
     const currentPath = window.location.pathname;
